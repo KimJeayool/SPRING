0372a235738a213f9e712d08b942b25806321ef2
jaeyoolkim <s09c073@gmail.com>
2023-03-26 15:45:24 +0900

    # 2개의 트랜잭션 사용 시 결과 확인

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -49,4 +49,37 @@ public class BasicTxTest {
         txManger.rollback(status);
         log.info("트랜잭션 롤백 완료");
     }
+
+
+    /**
+     * 'conn0' 커낵션을 같이 사용하지만, 커낵션을 다루는 프록시 객체와 주소가 다르다.
+     * 첫번째 : HikariProxyConnection@1448428389 wrapping conn0
+     * 두번째 : HikariProxyConnection@881578083 wrapping conn0
+     * */
+    @Test
+    void double_commit() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManger.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋 시작");
+        txManger.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManger.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋 시작");
+        txManger.commit(tx2);
+    }
+
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManger.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋 시작");
+        txManger.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManger.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋 시작");
+        txManger.rollback(tx2);
+    }
+
 }
